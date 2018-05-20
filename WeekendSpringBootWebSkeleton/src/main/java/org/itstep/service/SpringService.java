b2e0c71edef7beda0dc61b96692499da717b6a19
admin <admin@192.168.0.102>
2018-05-20 22:03:10 +0300

    final changes

---
+++ WeekendSpringBootWebSkeleton/src/main/java/org/itstep/service/SpringService.java
@@ -0,0 +1,17 @@
+package org.itstep.service;
+
+public interface SpringService {
+	
+	String getMethod1();
+	 
+	 String getMethod2();
+
+	 
+	 String putMethod();
+	 
+	 String postMethod();
+	 
+	 String deleteMethod();
+	
+
+}
